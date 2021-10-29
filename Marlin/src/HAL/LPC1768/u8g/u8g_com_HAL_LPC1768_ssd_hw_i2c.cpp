d771d5bd5fd96173f967aa1dcdf41b11a7379ac0
obertr0n <alex.balan@ymail.com>
2021-10-29 22:35:20 +0300

    sync with marlin repo

--- Marlin/src/HAL/LPC1768/u8g/u8g_com_HAL_LPC1768_ssd_hw_i2c.cpp
+++ Marlin/src/HAL/LPC1768/u8g/u8g_com_HAL_LPC1768_ssd_hw_i2c.cpp
@@ -79,7 +79,7 @@
 
 #if HAS_MARLINUI_U8GLIB
 
-#include <U8glib.h>
+#include <U8glib-HAL.h>
 
 #define I2C_SLA         (0x3C*2)
 //#define I2C_CMD_MODE  0x080
@@ -187,10 +187,6 @@ uint8_t u8g_com_HAL_LPC1768_ssd_hw_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_v
     case U8G_COM_MSG_ADDRESS:                     /* define cmd (arg_val = 0) or data mode (arg_val = 1) */
       u8g->pin_list[U8G_PI_A0_STATE] = arg_val;
       u8g->pin_list[U8G_PI_SET_A0] = 1;   /* force a0 to set again */
-//
-//      u8g_i2c_start(0); // send slave address and write bit
-//      u8g_i2c_send_byte(arg_val ? 0x40 : 0x80);  // Write to ? Graphics DRAM mode : Command mode
-      // u8g_SetPILevel(u8g, U8G_PI_A0, arg_val);
       break;
 
   } // switch
